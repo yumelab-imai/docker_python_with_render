14d372a041396552a6aa24926e9d0f7e457fd448
aki <aki@akis-MacBook-Air.local>
2023-08-06 16:22:34 +0900

    llm test12

--- main.py
+++ main.py
@@ -28,12 +28,12 @@ import requests
 import textract
 from pypdf import PdfReader
 from transformers import GPT2TokenizerFast
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
-from langchain.chains import ConversationalRetrievalChain
+# from langchain.text_splitter import RecursiveCharacterTextSplitter
+# from langchain.embeddings import OpenAIEmbeddings
+# from langchain.vectorstores import FAISS
+# from langchain.chains.question_answering import load_qa_chain
+# from langchain.llms import OpenAI
+# from langchain.chains import ConversationalRetrievalChain
 from data import pdf_urls
 import sys
 

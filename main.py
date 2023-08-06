780cc68a9a4e35e420afd4946a697514ad15767c
aki <aki@akis-MacBook-Air.local>
2023-08-06 16:29:44 +0900

    llm test991

--- main.py
+++ main.py
@@ -66,6 +66,82 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+
+
+
+
+
+
+
+    # ここから
+    # PDFのダウンロードと保存を行う関数
+    def download_and_save_pdf(url, filename):
+        response = requests.get(url)
+        if response.status_code == 200:
+            with open(filename, 'wb') as file:
+                file.write(response.content)
+        else:
+            print("Error: Unable to download the PDF file. The URL might be incorrect. Status code:", response.status_code)
+            sys.exit()
+
+    # PDFの読み込みを行う関数
+    def read_pdf(filename):
+        with open(filename, 'rb') as file:
+            page_contents = ''
+            reader = PdfReader(filename)
+            number_of_pages = len(reader.pages)
+            page_contents = ""
+            for page_number in range(number_of_pages):
+                pages = reader.pages
+                page_contents += pages[page_number].extract_text()
+
+            return page_contents
+
+
+
+
+
+    # 各PDFの全ページからデータを取得
+    pages_contents = ''
+    for i, url in enumerate(pdf_urls):
+        # PDFをダウンロードして保存
+        filename = f'sample_document{i+1}.pdf'
+        download_and_save_pdf(url, filename)
+        
+        # PDFを読み込み
+        pages_contents += read_pdf(filename)
+
+
+    chunks = pages_contents
+    print('URLのPDF群から情報を抽出しました。')
+    print(chunks)
+
+    text = chunks
+
+    # tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
+
+    # def count_tokens(text: str) -> int:
+    #     return len(tokenizer.encode(text))
+
+    # Split text into chunks
+    # text_splitter = RecursiveCharacterTextSplitter(
+    #     # Set a really small chunk size, just to show.
+    #     # chunk_size = 512,
+    #     chunk_size = 512,
+    #     chunk_overlap  = 24,
+    #     length_function = count_tokens,
+    # )
+
+    # chunks3 = text_splitter.create_documents([text])
+
+    print('step 222')
+    # print(chunks3)
+    # ここまで
+
+
+
+
+
     print('その３~4')
     # LINE bot => おうむ返し
     message = event.message.text

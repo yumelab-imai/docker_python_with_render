0d9a7f9f2c0b7d79a2ed13c8b9a8beba0b17876c
aki <aki@akis-MacBook-Air.local>
2023-08-08 08:55:50 +0900

    llm test993

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ import requests
 import textract
 from pypdf import PdfReader
 from transformers import GPT2TokenizerFast
-# from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 # from langchain.embeddings import OpenAIEmbeddings
 # from langchain.vectorstores import FAISS
 # from langchain.chains.question_answering import load_qa_chain
